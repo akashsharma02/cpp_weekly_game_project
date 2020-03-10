1822127cd69beb7a17d50f6b82b4fea3744c9f3f
Akash Sharma <akashsharma@cmu.edu>
2020-03-09 21:04:23 -0400

    Getting Started Ep1 Following along lefticus youtube series
    - Fork from akashsharma02 cpp_starter_project
    - Remove unneeded examples
    - Move ImGui-SFML examples into src/main.cpp
    - Demonstrate ImGui basics

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,102 @@
-#include <functional>
 #include <iostream>
+#include <vector>
 
 #include <spdlog/spdlog.h>
-
-
+#include <fmt/format.h>
 #include <docopt/docopt.h>
+
+#include <imgui.h>
+#include <imgui-SFML.h>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
 
 #include <iostream>
 
-static constexpr auto USAGE =
-  R"(Naval Fate.
+/* static constexpr auto USAGE = */
+/*   R"(Naval Fate. */
 
-    Usage:
-          naval_fate ship new <name>...
-          naval_fate ship <name> move <x> <y> [--speed=<kn>]
-          naval_fate ship shoot <x> <y>
-          naval_fate mine (set|remove) <x> <y> [--moored | --drifting]
-          naval_fate (-h | --help)
-          naval_fate --version
- Options:
-          -h --help     Show this screen.
-          --version     Show version.
-          --speed=<kn>  Speed in knots [default: 10].
-          --moored      Moored (anchored) mine.
-          --drifting    Drifting mine.
-)";
+/*     Usage: */
+/*           naval_fate ship new <name>... */
+/*           naval_fate ship <name> move <x> <y> [--speed=<kn>] */
+/*           naval_fate ship shoot <x> <y> */
+/*           naval_fate mine (set|remove) <x> <y> [--moored | --drifting] */
+/*           naval_fate (-h | --help) */
+/*           naval_fate --version */
+/*  Options: */
+/*           -h --help     Show this screen. */
+/*           --version     Show version. */
+/*           --speed=<kn>  Speed in knots [default: 10]. */
+/*           --moored      Moored (anchored) mine. */
+/*           --drifting    Drifting mine. */
+/* )"; */
 
-int main(int argc, const char **argv)
+int main([[maybe_unused]] int argc, [[maybe_unused]] const char **argv)
 {
-  std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
-    { std::next(argv), std::next(argv, argc) },
-    true,// show help if requested
-    "Naval Fate 2.0");// version string
+  /* std::map<std::string, docopt::value> args = docopt::docopt(USAGE, */
+  /*   { std::next(argv), std::next(argv, argc) }, */
+  /*   true,// show help if requested */
+  /*   "Naval Fate 2.0");// version string */
 
-  for (auto const &arg : args) {
-    std::cout << arg.first << arg.second << std::endl;
-  }
+  /* for (auto const &arg : args) { */
+  /*   std::cout << arg.first << arg.second << std::endl; */
+  /* } */
 
 
   //Use the default logger (stdout, multi-threaded, colored)
   spdlog::info("Hello, {}!", "World");
 
-  fmt::print("Hello, from {}\n", "{fmt}");
+  sf::RenderWindow window(sf::VideoMode(1024, 768), "ImGui + SFML = <3");
+  window.setFramerateLimit(60);
+  ImGui::SFML::Init(window);
+
+  constexpr auto scale_factor = 2.0f;
+  ImGui::GetStyle().ScaleAllSizes(scale_factor);
+  ImGui::GetIO().FontGlobalScale = scale_factor;
+
+
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Green);
+
+  bool state[11] = { false };
+
+  sf::Clock deltaClock;
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      ImGui::SFML::ProcessEvent(event);
+
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+
+    ImGui::SFML::Update(window, deltaClock.restart());
+
+    ImGui::Begin("The Plan");
+
+
+    ImGui::Checkbox("0 : The Plan", &state[0]);
+    ImGui::Checkbox("1 : Getting Started", &state[1]);
+    ImGui::Checkbox("2 : C++20 So Far", &state[2]);
+    ImGui::Checkbox("3 : Reading SFML Input States", &state[3]);
+    ImGui::Checkbox("4 : Managing Game State", &state[4]);
+    ImGui::Checkbox("5 : Making the Game Testable", &state[5]);
+    ImGui::Checkbox("6 : Making Game State Allocator Aware", &state[6]);
+    ImGui::Checkbox("7 : Add Logging to Game Engine", &state[7]);
+    ImGui::Checkbox("8 : Draw A Game Map", &state[8]);
+    ImGui::Checkbox("9 : Dialog Trees", &state[9]);
+    ImGui::Checkbox("10 : Porting from SFML to SDL", &state[10]);
+
+    ImGui::End();
+
+    window.clear();
+    window.draw(shape);
+    ImGui::SFML::Render(window);
+    window.display();
+  }
+
+  ImGui::SFML::Shutdown();
+  return 0;
 }
